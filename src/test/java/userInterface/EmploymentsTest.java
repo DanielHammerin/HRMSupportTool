9255607cbadd7ab37562a72620dde60161d6485a
Murredafuq <ma223ik@student.lnu.se>
2016-05-10 13:47:12 +0200

    Test - Employment part 1

--- src/test/java/userInterface/EmploymentsTest.java
+++ src/test/java/userInterface/EmploymentsTest.java
@@ -5,10 +5,11 @@
     in order to achive as much code coverage as possible.
  */
 package userInterface;
-import Model.Employments;
+import Model.Employment;
 import Model.FileRepo.*;
 import static org.junit.Assert.*;
 
+import View.DemoApplication;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,17 +24,21 @@ public class EmploymentsTest {
 
     private String companyId = "3251x5";
     private String personId = "9512352";
+    private int rowID = 12312313;
     private String EmploymentId = "0123Cs4";
     private String lastName = "Lyconhold";
-    private String firstName = "Markus";
-    private Employments employments = new Employments(companyId, personId, EmploymentId, lastName, firstName);
+    String FirstName = "Markus";
+
+    private Employment employments = new Employment(companyId, personId, EmploymentId, rowID, lastName, FirstName);
 	@Test
 	public void CompanyIdTest() {
-            Assert.assertTrue(employments.getCompanyId().equals(companyId));
+            Assert.assertTrue(employments.getCompanyID().equals(companyId));
 
 	}
     @Test
     public void FirstNameTest() {
+         String firstName = "Markus";
+
         Assert.assertTrue(employments.getFirstName().equals(firstName));
     }
     @Test
