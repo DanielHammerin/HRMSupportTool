93ed1294ab3215b5d8cb560c352e1a7a9d21e09f
Simon <simon44360@gmail.com>
2016-05-28 11:45:59 +0200

    File repo package javadoc

--- src/main/java/Model/FileRepo/UserFileRepository.java
+++ src/main/java/Model/FileRepo/UserFileRepository.java
@@ -16,9 +16,12 @@ import java.util.logging.Logger;
  * Updated by Hatem Houssein 4/28/2016 (Changed the logFilePath to "demo\LocalDatabase\Users_DB.ser")
  */
 public class UserFileRepository implements IRepository{
-   // private final String prefixPath = "D:\\HRM_SupportTool_Users\\";
+
     private final String UserFilePath = "LocalDatabase" + File.separator + "Users_DB.ser";
 
+    /**
+     * Constructor of the user file repository
+     */
     public UserFileRepository() {
         try {
             Path path = Paths.get(UserFilePath);
@@ -33,12 +36,21 @@ public class UserFileRepository implements IRepository{
         }
     }
 
+    /**
+     * Method to check if the user file exists
+     * @param path the file path
+     * @return true if the file exists, false otherwise
+     */
     private boolean isFileExist(String path) {
         return new File(path).exists();
     }
 
 
-
+    /**
+     * Method to create a user
+     * @param user the user to create
+     * @return true if the user creation has suceeded, false otherwise
+     */
     @Override
     public boolean createUser(User user) {
         List<User> users = readUsers();
@@ -46,6 +58,11 @@ public class UserFileRepository implements IRepository{
         return overwriteUsers(users);
     }
 
+    /**
+     * Method to overwrite a list of users
+     * @param users the list of users to overwrite
+     * @return true if the overwrite has suceeded, false otherwise
+     */
     private boolean overwriteUsers(List<User> users) {
         if (isFileExist(UserFilePath)) {
             try {
@@ -57,17 +74,18 @@ public class UserFileRepository implements IRepository{
             } catch (IOException i) {
                 return false;
             }
-
             return true;
         }
-
         return false;
     }
 
+    /**
+     * Method to get all the users
+     * @return the list of all users registred
+     */
     @Override
     public List<User> readUsers() {
         List<User> users = new ArrayList<>();
-
         if (isFileExist(UserFilePath) && !isEmptyFile(UserFilePath)) {
             try {
                 FileInputStream fileIn = new FileInputStream(UserFilePath);
@@ -78,14 +96,24 @@ public class UserFileRepository implements IRepository{
             } catch (Exception i) {
             }
         }
-
         return users;
     }
 
+    /**
+     * Methdo to check if the user file is empty
+     * @param userFilePath the file path
+     * @return true if the file exists, false otherwise
+     */
     private boolean isEmptyFile(String userFilePath) {
         return new File(userFilePath).length() == 0;
     }
 
+    /**
+     * Method to update a user
+     * @param userId the user id
+     * @param updateUser the new user object
+     * @return true if the update has suceeded, false otherwise
+     */
     @Override
     public boolean updateUser(int userId, User updateUser) {
         List<User> users = readUsers();
@@ -107,6 +135,11 @@ public class UserFileRepository implements IRepository{
         return overwriteUsers(users);
     }
 
+    /**
+     * Method to delete a user
+     * @param userId the user id
+     * @return true if the deletion has suceeded, false otherwise
+     */
     @Override
     public boolean deleteUser(int userId) {
         List<User> users = readUsers();
@@ -115,20 +148,23 @@ public class UserFileRepository implements IRepository{
         if (index < 0) {
             return false;
         }
-
         users.remove(index);
         return overwriteUsers(users);
     }
 
+    /**
+     * Method to get a member index
+     * @param userId the user id
+     * @param users the list of users registred
+     * @return the member index if the user exists in the list, -1 otherwise
+     */
     private int getMemberIndex(int userId, List<User> users) {
         for (int i = 0; i < users.size(); i++) {
             User user = users.get(i);
-
             if (user.getId() ==userId) {
                 return i;
             }
         }
-
         return -1;
     }
 }
